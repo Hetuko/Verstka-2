e776fdd266240bb6e7a077b26599a63e61819398
Valentin <wallpaperlowup@gmail.com>
2024-02-17 15:21:49 +0200

    Finished it, solved problem with fonts and media responses.

--- js/main.js
+++ js/main.js
@@ -6,9 +6,11 @@ const menuShadow = document.querySelector('.menu--close');
 menuBtn.addEventListener('click', () => {
     menuList.classList.toggle('menu__list--open');
     menuShadow.classList.toggle('menu--open');
+    document.body.classList.toggle('no-scroll');
 });
 
 menuClose.addEventListener('click', () => {
     menuList.classList.remove('menu__list--open');
     menuShadow.classList.remove('menu--open');
+    document.body.classList.remove('no-scroll');
 });

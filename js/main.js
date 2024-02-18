85a1134b0196d5b117f310890c1528ee60ed42a9
Valentin <wallpaperlowup@gmail.com>
2024-02-18 17:03:50 +0200

    Fix menu. On some devices you can't see the rest of the drop-down menu

--- js/main.js
+++ js/main.js
@@ -7,25 +7,10 @@ menuBtn.addEventListener('click', () => {
     menuList.classList.toggle('menu__list--open');
     menuShadow.classList.toggle('menu--open');
     document.body.classList.toggle('no-scroll');
-    handleMenuScroll();
 });
 
 menuClose.addEventListener('click', () => {
     menuList.classList.remove('menu__list--open');
     menuShadow.classList.remove('menu--open');
     document.body.classList.remove('no-scroll');
-    handleMenuScroll();
 });
-
-function handleMenuScroll() {
-    const menuHeight = menuList.scrollHeight;
-    const windowHeight = window.innerHeight;
-
-    if (menuHeight > windowHeight) {
-        menuList.style.height = '100vh'
-        console.log("height on")
-    }
-    else menuList.style.removeProperty('height');
-}
-
-window.addEventListener('resize', handleMenuScroll);

68024df0343339180d0ca0bb50b53fc9af01cd9e
Valentin <wallpaperlowup@gmail.com>
2024-02-18 18:12:06 +0200

    update mobile menu

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 const menuBtn = document.querySelector('.menu__btn');
 const menuClose = document.querySelector('.menu__close');
 const menuList = document.querySelector('.menu__list');
-const menuShadow = document.querySelector('.menu--close');
 
 menuBtn.addEventListener('click', () => {
     menuList.classList.toggle('menu__list--open');
-    menuShadow.classList.toggle('menu--open');
     document.body.classList.toggle('no-scroll');
 });
 
 menuClose.addEventListener('click', () => {
     menuList.classList.remove('menu__list--open');
-    menuShadow.classList.remove('menu--open');
     document.body.classList.remove('no-scroll');
 });
